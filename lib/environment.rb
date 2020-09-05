797ea8e89fcf8caa7f1d7fc068130a8388cb7287
caitlinhinshaw <caitlin.hinshaw@gmail.com>
2020-09-04 22:25:48 -0600

    Require JSON and Unirest for calling API

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,8 @@ require_relative "./word_lookup/version"
 require_relative "./word_lookup/cli"
 
 require 'pry'
+require 'json'
+require 'unirest'
 
 module WordLookup
   class Error < StandardError; end
