9e5931d28f95ecc177ac75e224dd56b2ba88487e
caitlinhinshaw <caitlin.hinshaw.dev@gmail.com>
2020-09-18 00:25:50 -0600

    Add API class back after sanitizing commit history

---
+++ lib/word_lookup/api.rb
@@ -0,0 +1,33 @@
+class WordLookup::API
+    def API_call(url)
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(url)
+        request["x-rapidapi-host"] = 'wordsapiv1.p.rapidapi.com'
+        request["x-rapidapi-key"] = 'YOUR_API_KEY_HERE'
+        
+        response = http.request(request)
+    end
+
+    def fetch_word_details_from_API(word)
+        url = URI("https://wordsapiv1.p.rapidapi.com/words/#{word}/")
+
+        json = API_call(url)
+
+        convert_json_to_ruby(json)
+    end
+
+    def fetch_rhymes_from_API(word)
+        url = URI("https://wordsapiv1.p.rapidapi.com/words/#{word}/rhymes")
+
+        json = API_call(url)
+
+        convert_json_to_ruby(json)
+    end
+
+    def convert_json_to_ruby(json)
+        JSON.parse(json.body)
+    end
+end
